734f6beaddc3910411c14c980324a1a07fcb52dd
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-08-05 20:55:50 -0500

    Update repo.js

--- js/repo.js
+++ js/repo.js
@@ -25,6 +25,7 @@ function displayApps(apps) {
         <p><strong>Bundle ID:</strong> ${app.bundle_id}</p>
         <p><strong>Version:</strong> ${app.version}</p>
         <a class="download-button" href="${app.download}">Download IPA</a>
+        <a class="download-button" href="${app.dowmload}">Instant Download(if have)</a>
       </div>
     `;
     container.appendChild(card);
