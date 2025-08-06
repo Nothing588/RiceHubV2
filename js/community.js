529632ae200df5c5a17f1ad4f849bc81cd23a926
VietRiceLord <144397456+RiceEater15@users.noreply.github.com>
2025-08-06 12:54:35 -0500

    Update community.js

--- js/community.js
+++ js/community.js
@@ -1,14 +1,18 @@
 let allApps = [];
 
+document.getElementById('loading-spinner').style.display = 'block'; 
+
 fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiOW63Pc6Kaw5jZzY0Cs5ol_NnMlhA73afWlHWukblObyOKxoUGWeiqZD9R5SWhp_rgs6OTPzPCyY5zmR_b-q2sWiB7JpUz2C6eUucztAgjxKS9m0CAuyErVqJ79ms5WGMXC14rD7Wuhuwswat33HVLpwcFdm2S9IcAi06FZuvX2vNs0wsmhQ7-cATi732fFQmX7p3sZQCQ-lwIgDBxT0vgYsqF-m5Q_83Nq7pUquN0N-LdACuNz782zjfjXFUkfkgy9sKtA2KQtmJlHYj7UESDOs5ltQ&lib=M-6L6s3vj7kNcfkGIWeID7ErknCf72OHO')
   .then(res => res.json())
   .then(apps => {
     allApps = apps;
     displayApps(apps);
+    document.getElementById('loading-spinner').style.display = 'none'; 
   })
   .catch(err => {
     console.error("Failed to load apps JSON:", err);
     document.getElementById("app-list").innerHTML = "<p>Failed to load app list.</p>";
+    document.getElementById('loading-spinner').style.display = 'none'; 
   });
 
 function displayApps(apps) {
